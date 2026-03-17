33d5fea3e93e6ff1b9a780a3bc60897248a9f2d5
lavozdemanaureapp <alexisemch0@gmail.com>
2026-03-17 11:56:34 -0400

    Add files via upload

---
+++ aplicación/sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'manaure-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/micambio.html',
+  '/canales.html',
+  '/PROGRAMAS.html', // <--- AGREGA ESTA LÍNEA EXACTAMENTE ASÍ
+  '/css/estilos.css',
+  '/js/script.js'
+];
